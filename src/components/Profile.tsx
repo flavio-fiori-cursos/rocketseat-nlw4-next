35fb007f98ef4be8375bd03bdd422af3b52907f9
Flávio <flavio.fiori31@gmail.com>
2021-02-23 11:02:23 -0300

    :tada: Adiciona aula 02 nlw#4

---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import styles from './../styles/components/Profile.module.css';
+
+export function Profile() {
+
+    return(
+
+        <div className={styles.profileContainer}>
+
+            <img src="https://github.com/flaviofiori.png" alt="Flávio Fiori" />
+
+            <div>
+
+                <strong>Flávio Fiori</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 1
+                </p>
+
+            </div>
+
+        </div>
+
+    )
+
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+
+import Head from 'next/head';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
+
+import { ExperienceBar } from '../components/ExperinceBar';
+import { Profile } from '../components/Profile';
+
+import styles from '../styles/pages/Home.module.css';
+
+export default function Home() {
+    return (
+        
+        <div className={styles.container}>
+
+            <Head>
+                <title>Início | MoveIt</title>
+            </Head>
+
+            <ExperienceBar />
+
+            <section>
+
+                <div>
+                    
+                    <Profile />
+
+                    <CompletedChallenges />
+
+                    <Countdown />
+
+                </div>
+
+            </section>
+
+
+
+        </div>
+    )
+}
